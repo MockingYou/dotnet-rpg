d9433069d8c2e5bd560ccf37ef156513dacb84e2
Vlad <vlad.mocanu13@gmail.com>
2024-02-17 20:22:08 +0200

    - added connection to mysql database

--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
 global using dotnet_rpg.Models;
 global using dotnet_rpg.Services.CharacterService;
+global using dotnet_rpg.Dtos.Character;
+global using AutoMapper;
+global using Microsoft.EntityFrameworkCore;
+global using dotnet_rpg.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<DataContext>(options => 
+    options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddScoped<ICharacterService, CharacterService>();
 var app = builder.Build();
 
--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -2,39 +2,87 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 
 namespace dotnet_rpg.Services.CharacterService
 {
     public class CharacterService : ICharacterService
     {
-        private static List<Character> characters  = new List<Character> {
-            new Character(),
-            new Character{ Id = 1, Name = "Gigel" }
-        };
-        public async Task<ServiceResponse<List<Character>>> GetAllCharacters()
+        private readonly List<Character> _characters = new List<Character>
         {
-            var serviceResponse = new ServiceResponse<List<Character>>();
-            serviceResponse.Data = characters;
+            new Character(),
+            new Character { Id = 1, Name = "Gigel" }
+        };
+        private readonly IMapper _mapper;
+        private readonly DataContext _context;
+        public CharacterService(IMapper mapper, DataContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+        public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
+        {
+            var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
+            var dbCharacters = await _context.Characters.ToListAsync();
+            serviceResponse.Data = dbCharacters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
             return serviceResponse;
         }
-
-        public async Task<ServiceResponse<Character>> GetCharacterById(int id)
+        public async Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id)
         {
-            var character = characters.FirstOrDefault(c => c.Id == id );
-            if(character is not null) {
-                var serviceResponse = new ServiceResponse<Character>();
-                serviceResponse.Data = character;
+            var character = _characters.FirstOrDefault(c => c.Id == id);
+            if (character != null)
+            {
+                var serviceResponse = new ServiceResponse<GetCharacterDto>();
+                serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
                 return serviceResponse;
             }
-            throw new Exception("Character not found");
+            return new ServiceResponse<GetCharacterDto> { Message = "Character not found" };
         }
 
-        public async Task<ServiceResponse<List<Character>>> AddCharacter(Character newCharacter)
+        public async Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter)
         {
-            var serviceResponse = new ServiceResponse<List<Character>>();
-            characters.Add(newCharacter);
-            serviceResponse.Data = characters;
+            var character = _mapper.Map<Character>(newCharacter);
+            character.Id = _characters.Max(c => c.Id) + 1;
+            _characters.Add(character);
+
+            var serviceResponse = new ServiceResponse<List<GetCharacterDto>>
+            {
+                Data = _characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList()
+            };
             return serviceResponse;
+        }
+        public async Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto updatedCharacter)
+        {
+            var serviceResponse = new ServiceResponse<GetCharacterDto>();
+            try 
+            {
+                var character = _characters.FirstOrDefault(c => c.Id == updatedCharacter.Id) ?? throw new Exception($"Character with Id '{updatedCharacter.Id}' not found. ");
+                _mapper.Map(updatedCharacter, character);
+                serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
+            } 
+            catch (Exception ex)
+            {
+                serviceResponse.Success = false;
+                serviceResponse.Message = ex.Message;
+            }
+            return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<List<GetCharacterDto>>> DeleteCharacter(int id)
+        {
+            var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
+            try 
+            {
+                var character = _characters.FirstOrDefault(c => c.Id == id) ?? throw new Exception($"Character with Id '{id}' not found. ");
+                _characters.Remove(character);
+                serviceResponse.Data = _characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+            } 
+            catch (Exception ex)
+            {
+                serviceResponse.Success = false;
+                serviceResponse.Message = ex.Message;
+            }
+            return await Task.FromResult(serviceResponse);
         }
     }
 }
